f3407d373f5836f75e9b91474fa7ea680440276f
Tom Maiaroto <tom.maiaroto@gmail.com>
2018-03-26 23:33:22 -0700

    version bump

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@
 package version
 
 // Semantic defines a semver string for aegis
-const Semantic = "1.3.1"
+const Semantic = "1.4.0"
 
 // Current will return the current version
 func Current() string {
